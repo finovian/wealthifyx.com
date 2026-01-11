53099573f8ab4f06d97d7286dc47ec9e21ab92cf
finovian <jayrajshakha01@gmail.com>
2026-01-11 14:19:05 +0530

    Update UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
 import { generateWebsiteSchema } from "@/lib/schema";
@@ -22,23 +23,36 @@ export default function RootLayout({
         <link rel="preconnect" href="https://www.googletagmanager.com" />
         <link rel="preconnect" href="https://www.google-analytics.com" />
         <link rel="icon" href="/favicon.svg" />
-        <script defer src="https://www.googletagmanager.com/gtag/js?id=G-6PR1F8E717" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-6PR1F8E717');
-            `,
-          }}
+
+        {/* GA4 base script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6PR1F8E717"
+          strategy="afterInteractive"
         />
+
+        {/* GA4 init */}
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+
+            gtag('js', new Date());
+            gtag('config', 'G-6PR1F8E717', {
+              debug_mode: true
+            });
+          `}
+        </Script>
       </head>
+
       <body className="min-h-screen bg-white text-gray-900 antialiased">
         <script
           type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(generateWebsiteSchema()) }}
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(generateWebsiteSchema()),
+          }}
         />
+
         <Navbar />
         <main className="mx-auto max-w-6xl px-4">{children}</main>
         <Footer />
